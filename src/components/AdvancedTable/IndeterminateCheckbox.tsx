1b8235e3d4054fe810983828def1c0956cfb2aea
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2025-03-24 18:08:52 +0400

    fix: added actions list

--- src/components/AdvancedTable/IndeterminateCheckbox.tsx
+++ src/components/AdvancedTable/IndeterminateCheckbox.tsx
@@ -3,6 +3,10 @@ import type { ForwardedRef } from 'react'
 import React, { forwardRef, useEffect, useRef } from 'react'
 import { Checkbox } from '../Checkbox'
 import classNames from 'classnames'
+import {Button} from "../Button";
+import IconDelete from "../SVGIcons/IconDelete";
+import IconSend from "../SVGIcons/IconSend";
+import IconEdit from "../SVGIcons/IconEdit";
 
 type IndeterminateCheckboxProps = {
   indeterminate: boolean
@@ -28,13 +32,20 @@ export const IndeterminateCheckbox = forwardRef(
     }
 
     return (
-      <Checkbox
-        className={classNames('checkbox', { 'active-checkbox': checked })}
-        value={checked}
-        disabled={disabled}
-        onClick={handleChange}
-        ref={resolvedRef}
-      />
+        <div className={'actions-list'}>
+          <Checkbox
+              className={classNames('actions-list__checkbox', { 'active-checkbox': checked })}
+              value={checked}
+              disabled={disabled}
+              onClick={handleChange}
+              ref={resolvedRef}
+          />
+          <div className={'actions-list__right'}>
+            <Button iconProps={{Component: IconDelete}} type={'secondary'} size={'medium'}/>
+            <Button iconProps={{Component: IconSend}} type={'secondary'} size={'medium'}/>
+            <Button iconProps={{Component: IconEdit}} type={'secondary'} size={'medium'}/>
+          </div>
+        </div>
     )
   }
 )
