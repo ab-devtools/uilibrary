ada740f8daf9525bd937440457469f894a71d86a
valodyap <valodya.papikyan@ameriabank.am>
2025-04-17 14:23:00 +0400

    fix: add pin and hover functionality

--- src/components/SideNavigation/SideNavigation.tsx
+++ src/components/SideNavigation/SideNavigation.tsx
@@ -1,6 +1,6 @@
 import type { TSideNavigationPropTypes } from './types'
 import type { JSX } from 'react'
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
 import { ButtonIcon } from '../ButtonIcon'
 import IconPanelRight from '../SVGIcons/IconPanelRight'
@@ -8,36 +8,44 @@ import IconPanelLeft from '../SVGIcons/IconPanelLeft'
 import { Image } from '../Image'
 
 export const SideNavigation = (props: TSideNavigationPropTypes): JSX.Element => {
-  const {
-    children,
-    isOpen = true,
-    setOpen,
-    pinSidebar = true,
-    className = '',
-    logo,
-    logoClosed
-  } = props
+  const { children, isOpen = true, setOpen, className = '', logo, logoClosed } = props
 
-  const handleToggleOpen = () => {
-    setOpen?.(!isOpen)
+  const [isPined, setPined] = useState(true)
+
+  const onMouseEnter = () => {
+    if (isPined) {
+      setOpen?.(true)
+    }
+  }
+
+  const onMouseLeave = () => {
+    if (isPined) {
+      setOpen?.(false)
+    }
+  }
+
+  const onPin = () => {
+    setPined(!isPined)
   }
 
   return (
     <div
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
       className={classNames(
         'side-navigation',
         isOpen ? 'side-navigation--opened' : null,
-        pinSidebar ? 'side-navigation--pin' : null,
+        isPined ? 'side-navigation--pin' : null,
         className
       )}
     >
       <div className="side-navigation__header">
         <Image imagePath={isOpen ? logo : logoClosed} isBackgroundImage={false} />
         <ButtonIcon
-          iconProps={{ Component: pinSidebar ? IconPanelRight : IconPanelLeft }}
+          iconProps={{ Component: isPined ? IconPanelRight : IconPanelLeft }}
           size={'large'}
           className={'side-navigation__btn'}
-          onClick={handleToggleOpen}
+          onClick={onPin}
         />
       </div>
       {children}
