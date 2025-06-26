aff6179d1d650bc9e498025f298e3e72a47f46b7
karoghulyan <kghulyan93@gmail.com>
2025-06-26 18:04:45 +0400

    feat: check modal buttons

--- src/components/Modal/ModalConfirmation.tsx
+++ src/components/Modal/ModalConfirmation.tsx
@@ -89,13 +89,17 @@ export const ModalConfirmation = (props: TModalConfirmationPropTypes): ReactElem
                     isFooterInline ? 'modal__footer-inline' : 'modal__footer-block'
                   }`}
                 >
-                  <Button
-                    type="secondary"
-                    className="modal__footer__btn"
-                    onClick={onClose}
-                    dataId={dataIdPrefix ? `${dataIdPrefix}-modal-cancel-button` : ''}
-                    {...(buttonProps?.cancel || {})}
-                  />
+                  {
+                    buttonProps.cancel ? (
+                      <Button
+                        type="secondary"
+                        className="modal__footer__btn"
+                        onClick={onClose}
+                        dataId={dataIdPrefix ? `${dataIdPrefix}-modal-cancel-button` : ''}
+                        {...(buttonProps?.cancel || {})}
+                      />
+                    ) : null
+                  }
                   {buttonProps?.deactivate ? (
                     <Button
                       type="secondary"
@@ -105,13 +109,17 @@ export const ModalConfirmation = (props: TModalConfirmationPropTypes): ReactElem
                       {...buttonProps?.deactivate}
                     />
                   ) : null}
-                  <Button
-                    type="danger"
-                    className={'modal__footer__btn'}
-                    onClick={onSubmit}
-                    dataId={dataIdPrefix ? `${dataIdPrefix}-modal-confirm-button` : ''}
-                    {...buttonProps.confirm}
-                  />
+                  {
+                    buttonProps.confirm ? (
+                      <Button
+                        type="danger"
+                        className={'modal__footer__btn'}
+                        onClick={onSubmit}
+                        dataId={dataIdPrefix ? `${dataIdPrefix}-modal-confirm-button` : ''}
+                        {...buttonProps.confirm}
+                      />
+                    ) : null
+                  }
                 </div>
               ) : null}
             </div>
