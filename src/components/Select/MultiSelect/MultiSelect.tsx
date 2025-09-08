040d9c8fe3639eeb808d07138538692641bbb9ed
Luiza Shirmazanyan <luizashirmazanyan@Luizas-MacBook-Pro.local>
2025-09-08 16:59:53 +0400

    feat: conditional closeDropdown for multiselect

--- src/components/Select/MultiSelect/MultiSelect.tsx
+++ src/components/Select/MultiSelect/MultiSelect.tsx
@@ -102,7 +102,11 @@ export const MultiSelect = (props: TMultiSelectPropTypes): ReactElement => {
     useId()
   )
 
-  const submitSelectedValue = (selections: TSelectedValue[], isChecked: boolean) => {
+  const submitSelectedValue = (
+    selections: TSelectedValue[],
+    isChecked: boolean,
+    closeDropdownOnApply = true
+  ) => {
     if (setSelectedItems) {
       setSelectedItems(selections, isChecked)
     }
@@ -110,7 +114,9 @@ export const MultiSelect = (props: TMultiSelectPropTypes): ReactElement => {
       setFieldValue(name, selections)
     }
 
-    closeDropdown()
+    if (closeDropdownOnApply) {
+      closeDropdown()
+    }
   }
 
   const applySelectedItems = (isChecked: boolean) => {
