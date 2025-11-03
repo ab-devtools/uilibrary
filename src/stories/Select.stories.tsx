44d4492155b6939febf6684d2b955230395fc4ab
Garik Grigoryan <garikgrigoryan@Gariks-MacBook-Pro.local>
2025-11-03 16:55:12 +0400

    fix: select outside click empty value

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -318,7 +318,7 @@ const languagesOptions = [
 
 // -----------SINGLE SELECT---------
 const Template: StoryFn<TSingleSelectPropTypes> = (args) => {
-  const [selectedValue, setSelectedValue] = useState<TItemValue | undefined>(null)
+  const [selectedValue, setSelectedValue] = useState<TItemValue | undefined>("1")
 
   return (
     <div style={{ display: 'flex', maxHeight: '100vh', justifyContent: 'center' }}>
@@ -333,6 +333,8 @@ const Template: StoryFn<TSingleSelectPropTypes> = (args) => {
           selectedItem={selectedValue}
           setSelectedItem={setSelectedValue}
           isSearchable={true}
+          isCreateOnOutsideClick
+          defaultValue="1"
           optionRightIconComponent={(value) => <>{value}</>}
         />
       </div>
