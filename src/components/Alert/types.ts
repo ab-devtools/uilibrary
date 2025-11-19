855badff88f9baf87348870b6b8ac5710905950d
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2025-11-19 11:29:51 +0400

    fix:  update alert ui

--- src/components/Alert/types.ts
+++ src/components/Alert/types.ts
@@ -1,12 +1,17 @@
 import type { JSX } from 'react'
 import type { TButtonPropTypes } from '../Button/types'
 import type { LinkPropTypes } from '../Link/types'
+import type { TSVGIconType } from '../SVGIcons/types'
 
 export type TAlertTypes = 'grey' | 'success' | 'error' | 'warning' | 'information' | 'discovery'
 export type TAlertPosition = 'inline' | 'sticky'
 
 export interface TAlertProps extends IFormCompProps {
   type?: TAlertTypes
+  iconProps?: {
+    type?: TSVGIconType
+    Component?: TSVGIconComponent
+  }
   position?: TAlertPosition
   text?: string | JSX.Element
   subtext?: string | JSX.Element
