a468e5f58602fea2e96fbd00748df868d56070ca
Ramella.Naltakyan <Ramella.Naltakyan@ameriabank.am>
2025-11-26 15:42:40 +0400

    fix: fixed framed icon bg

--- src/stories/FramedIcon.stories.tsx
+++ src/stories/FramedIcon.stories.tsx
@@ -4,6 +4,7 @@ import type { StoryFn } from '@storybook/react'
 import { FramedIcon as _FramedIcon } from '../index'
 import { TButtonIconPropTypes } from '../components/ButtonIcon/types'
 import IconDelete from '../components/SVGIcons/IconDelete'
+import {TFramedIconPropTypes} from "../components/FramedIcon/types";
 
 export default {
   title: 'FramedIcon',
@@ -12,15 +13,24 @@ export default {
     size: {
       options: ['large', 'medium', 'small'],
       control: { type: 'radio' }
+    },
+    type: {
+      options: ['secondary', 'brand-light', 'warning-light', 'danger-light', 'success-light', 'information-light'],
+      control: { type: 'radio' }
     }
   }
 }
 
-const Template: StoryFn<TButtonIconPropTypes> = (args) => (
+const Template: StoryFn<TFramedIconPropTypes> = (args) => (
   <div>
-    <_FramedIcon {...args} iconProps={{ Component: IconDelete, type: 'warning-light' }} />
+    <_FramedIcon {...args} />
   </div>
 )
 
 export const FramedIcon = Template.bind({})
-FramedIcon.args = {}
+FramedIcon.args = {
+    iconProps: {
+        Component: IconDelete ,
+        type: 'brand-light'
+    }
+}
