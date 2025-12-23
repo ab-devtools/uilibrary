675cfb55baee8b00d9724906ecab99f964fa9f38
Garik Grigoryan <garikgrigoryan@Gariks-MacBook-Pro.local>
2025-12-23 14:35:59 +0400

    fix: select options dropdown height

--- src/components/Select/Select/Select.tsx
+++ src/components/Select/Select/Select.tsx
@@ -297,13 +297,13 @@ export const Select = (props: TSingleSelectPropTypes): JSX.Element | null => {
             width,
             ...(hasBottomSpace
               ? {
-                top: bottom,
-                maxHeight: top - DROPDOWN_AND_INPUT_GAP
-              }
+                  top: bottom,
+                  maxHeight: top - DROPDOWN_AND_INPUT_GAP
+                }
               : {
-                bottom: window.innerHeight - top + DROPDOWN_AND_INPUT_GAP,
-                maxHeight: window.innerHeight - bottom - DROPDOWN_AND_INPUT_GAP
-              })
+                  bottom: window.innerHeight - top + DROPDOWN_AND_INPUT_GAP,
+                  maxHeight: window.innerHeight - bottom - DROPDOWN_AND_INPUT_GAP
+                })
           }}
           ref={setDropdownRef}
         >
